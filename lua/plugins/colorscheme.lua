59d398610a32cd3c2c5f1ad3f6a731d05d8b05da
Pablolinari <paablolinaari@gmail.com>
2024-06-05 19:45:04 +0200

    .

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+
+	-- Configure LazyVim to load gruvbox
+	{
+		"folke/tokyonight.nvim",
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
+}
